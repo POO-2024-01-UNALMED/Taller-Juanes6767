907b838857500147e7aed0bdda84cb94e22f0fb3
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:38:31 -0500

    Update Libro.java

--- src/main/java/comunicacion/Libro.java
+++ src/main/java/comunicacion/Libro.java
@@ -2,4 +2,11 @@ package comunicacion;
 
 public class Libro extends Escrito{
 
+  private String co_autor;
+  private String editorial;
+  private String edicion;
+  private String interpretacion;
+  
+  public String interpretacion(){
+    return this.interpretacion;
 }
