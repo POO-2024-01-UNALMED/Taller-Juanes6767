85ad67b7eee3b8f8ff9cc23c7d5dcfc80aa4bcd8
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:34:02 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -2,4 +2,9 @@ package comunicacion;
 
 public class Tesis extends Escrito{
 
+  private String idea;
+  private static ArrayList<String> argumentos; 
+  private String conclusion;
+  private String referencias;
+  private String interpretacion;
 }
