6bde87fe514c6778c6b9230f8907df16f802baf9
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 08:36:12 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -15,6 +15,10 @@ public class Alfabeto extends Pictograma{
   public String interpretacion(){
     return this.interpretacion;
   }
+  public static String toString(){
+    return "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+  }
+  
   public String[] getLetras(){
     return letras;
   }
