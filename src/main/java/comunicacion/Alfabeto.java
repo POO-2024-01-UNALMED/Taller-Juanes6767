f67294023033a438e007d21fc6a1009b25d598ff
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:29:46 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,5 +1,8 @@
 package comunicacion;
-
+import java.util.*
 public class Alfabeto extends Pictograma{
+  private static ArrayList<String> letras;
+  private String interpretacion;
 
+  int cantidadLetras(){}
 }
