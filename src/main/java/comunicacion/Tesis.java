c11f56b9741f679d2dac1c159a9af2cc669e9e39
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 09:22:25 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -23,7 +23,8 @@ public class Tesis extends Escrito{
     return this.interpretacion;
   }
   public String toString(){
-    return ""+super.origen+"\n"+super.titulo+"\n"+super.autor+"\n"+super.paginas+"\n"+this.idea+"\n"+argumentos.length+"\n"+this.conclusion"\n"+this.referencias+"";
+    String s=super.origen+"\n"+super.titulo+"\n"+super.autor+"\n"+super.paginas+"\n"+this.idea+"\n"+argumentos.length+"\n"+this.conclusion"\n"+this.referencias;
+    return s;
   }
   public String getIdea(){
     return this.idea;
