a38d0f555a0ed4aee36f85f847e593c052dc360f
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:47:45 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -3,4 +3,13 @@ package comunicacion;
 public class Fabula extends Escrito{
   private String ensenanza;
   private String interpretacion;
+
+  public String interpretacion(){
+    return this.interpretacion;
+  }
+  public String getEnsenanza(){
+    return this.ensenanza;
+  }
+  public void setLetras(String l){
+    this.ensenanza=l;
 }
