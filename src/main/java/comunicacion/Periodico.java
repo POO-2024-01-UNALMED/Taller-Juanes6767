b2f4211fedd5e350c7f314331ec7e8532b2aff3f
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:36:39 -0500

    Update Periodico.java

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -4,4 +4,8 @@ public class Periodico extends Escrito{
   private String fecha;
   private String primicia;
   private String interpretacion;
+
+  public String interpretacion(){
+    return this.interpretacion;
+  }
 }
