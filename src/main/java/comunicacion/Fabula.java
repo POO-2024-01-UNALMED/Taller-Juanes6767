cb89aef975e6a549f5af66b5e276429847941bdd
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 08:19:07 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -4,6 +4,11 @@ public class Fabula extends Escrito{
   private String ensenanza;
   private String interpretacion;
 
+  public Fabula(String origen,String titulo,String autor, int paginas,String en,String i){
+    super(origen, titulo, autor,paginas);
+    this.ensenanza=en;
+    this.interpretacion=i;
+  }
   public String interpretacion(){
     return this.interpretacion;
   }
