7528d6bcae655cbb3efdc9aeb074c06d351d17fd
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-07 07:54:01 -0500

    Update Periodico.java

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -8,4 +8,16 @@ public class Periodico extends Escrito{
   public String interpretacion(){
     return this.interpretacion;
   }
+  public String getFecha(){
+    return this.fecha;
+  }
+  public void setFecha(String letras){
+    this.fecha=letras;
+  }
+  public String getPrimicia(){
+    return this.primicia;
+  }
+  public void setPrimicia(String letras){
+    this.primicia=letras;
+  }
 }
